dc1d9546159a2409c39e983d57f7932e64708191
Matheus Carnelutt <matheuschafrao@gmail.com>
2018-04-20 12:43:29 -0300

    Arrumando TAD Arquivo

--- src/arquivo.h
+++ src/arquivo.h
@@ -1,17 +1,55 @@
 #ifndef __ARQUIVO_H__
 #define __ARQUIVO_H__
 
+/**
+ * Serve para guardar e como interface de um arquivo aberto, podendo ser de
+ * leitura ou escrita.
+ */
 typedef void *Arquivo;
-typedef int TipoArquivo;
 
-Arquivo abrir_arquivo(char *path, TipoArquivo modo);
+enum TipoArquivo { LEITURA, ESCRITA, ALTERACAO };
 
+/**
+ * 'path' deve ser um caminho valido.
+ * Retorna o arquivo aberto em path, válido para a operação escolhida.
+ */
+Arquivo abrir_arquivo(char *path, enum TipoArquivo modo);
+
+/**
+ * 'a' deve ser do modo leitura
+ * retorna a proxima linha do arquivo a;
+ */
 char *ler_proxima_linha(Arquivo a);
 
-char *escrever_linha(Arquivo a, char *entrada);
+/**
+ * 'a' deve ser do modo escrita ou alteracao
+ * Escreve em 'a' a string 'entrada'
+ */
+void escrever_linha(Arquivo a, char *entrada);
 
+/**
+ * Retorna a linha atual do arquivo 'a'
+ */
 int numero_linha_atual(Arquivo a);
 
+/**
+ * Fecha o arquivo 'a'. Ele deve ter sido aberto anteriormente.
+ */
 void fechar_arquivo(Arquivo a);
+
+/**
+ * Retorna o nome do arquivo
+ */
+char *get_nome(Arquivo a);
+
+/**
+ * Retorna o diretorio do arquivo
+ */
+char *get_diretorio(Arquivo a);
+
+/**
+ * Retorna a extensao do arquivo
+ */
+char *get_extensao(Arquivo a);
 
 #endif  // __ARQUIVO_H__
