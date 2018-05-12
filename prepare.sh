81e339222dcd1ca13369ef2a694fb24790885970
Matheus Carnelutt <matheuschafrao@gmail.com>
2018-05-12 16:56:19 -0300

    Adicionado script de gerar arquivo zip para enviar.

---
+++ prepare.sh
@@ -0,0 +1,23 @@
+NAME='mcchafrao'
+
+echo "Criando arquivo \"$NAME.zip\" para enviar."
+# Cria pasta
+mkdir -p $NAME
+
+# Copia os arquivos necessarios
+cp -r src $NAME/
+cp LEIA-ME.txt $NAME/
+cp Makefile $NAME/src/
+
+# Muda os parametros do Makefile
+sed -i -e 's/= src/= ./g' $NAME/src/Makefile
+sed -i -e 's/= build/= ..\/build/g' $NAME/src/Makefile
+sed -i -e 's/= bin/= ./g' $NAME/src/Makefile
+
+# Compacta a pasta
+zip $NAME -r $NAME/ -q
+
+# Remove a pasta
+rm -rf $NAME/
+
+echo "Processo concluido!"
