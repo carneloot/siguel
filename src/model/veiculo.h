f27c9b3557d8c83701cda2c56fa784cd4b02a9ca
Ragnaranger <danbranlent@yahoo.com.br>
2018-11-06 15:02:37 -0200

    Montando veiculo.h

--- src/model/veiculo.h
+++ src/model/veiculo.h
@@ -1,9 +1,18 @@
 #ifndef __CARRO_H__
 #define __CARRO_H__
 
+
+#include <stdbool.h>
+
 #include "modules/ponto2d.h"
 
-typedef struct Veiculo *Veiculo
+typedef void* Veiculo;
+
+Veiculo cria_veiculo( Ponto2D posicao, double w, double h, char *placa );
+
+bool colide_veiculo( Veiculo v1, Veiculo v2 );
+
+
 
 
 #endif /* __CARRO_H__ */
