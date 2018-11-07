6baa6c2190349fe8a082692d7d15ceeefa267cd4
Matheus Carnelutt <matheuschafrao@gmail.com>
2018-11-06 23:39:41 -0200

    Comando "@g?" implementado.

--- src/model/comando.c
+++ src/model/comando.c
@@ -54,6 +54,7 @@
   ENTRY( comando_qry_hmpe, "qry/hmpe?") \
   ENTRY(  comando_qry_dpr,   "qry/dpr") \
   ENTRY(    comando_reg_m,   "qry/@m?") \
+  ENTRY(    comando_reg_g,   "qry/@g?") \
   ENTRY(     comando_ec_t,      "ec/t") \
   ENTRY(     comando_ec_e,      "ec/e") \
   ENTRY(     comando_pm_p,      "pm/p") \
