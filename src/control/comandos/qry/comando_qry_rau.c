8454d8bb62c7721558750848332172bddda19937
Ragnaranger <danbranlent@yahoo.com.br>
2018-11-06 22:12:26 -0200

    Comando remover veículo praticamente pronto, falta remover placa do svg. (Ainda não foi implementado o inserir placa no svg)

--- src/control/comandos/qry/comando_qry_rau.c
+++ src/control/comandos/qry/comando_qry_rau.c
@@ -13,3 +13,37 @@
 
 
 */
+#include <model/comando.r>
+#include <control/controlador.r>
+
+#include <model/veiculo.h>
+#include <model/modules/lista.h>
+#include <model/modules/logger.h>
+
+int comando_qry_rau( void* _this, void* _controlador ){
+  struct Comando *this = _this;
+  struct Controlador *controlador = _controlador;
+
+  Posic atual = Lista_t.get_first(controlador->veiculos);
+
+  // Desculpaaa, só vi que tinha search depois
+  while( !strcmp( this->params[0], 
+                  get_placa_veiculo( Lista_t.get(controlador->veiculos, atual))
+                  ) == 0 )
+  {
+    atual = Lista_t.get_next;
+  }
+
+  if( atual == NULL){
+    LOG_ERRO(LOG_FILE, "Tendando remover um veiculo que nao existe");
+    return 0;
+  }
+
+  Lista_t.remove(controlador->veiculos, atual);
+
+  Lista_t.remove(controlador->figuras, get_figura_veiculo(Lista_t.get(controlador->veiculos, atual)) );
+
+  // TODO: Remover a placa do carro do SVG
+
+  return 1;
+}
--- src/model/comando.c
+++ src/model/comando.c
@@ -55,6 +55,7 @@
   ENTRY(  comando_qry_dpr,   "qry/dpr") \
   ENTRY(tire_esse_comando,   "qry/tec") \
   ENTRY(   comando_qry_au,    "qry/au") \
+  ENTRY(  comando_qry_rau,    "qry/au") \
   ENTRY(     comando_ec_t,      "ec/t") \
   ENTRY(     comando_ec_e,      "ec/e") \
   ENTRY(     comando_pm_p,      "pm/p") \
