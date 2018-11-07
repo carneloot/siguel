852016337159cf8cebd651f906858b006626b4b7
Matheus Carnelutt <matheuschafrao@gmail.com>
2018-11-07 13:06:47 -0200

    Pequenas melhorias. - Mudado para InfoG ao inves de void * - Se vertices ja foi definido, nao pode redefinir - Adicionado numero e maximo de vertices

--- src/model/modules/grafod.c
+++ src/model/modules/grafod.c
@@ -8,13 +8,13 @@
 #include "kdtree.h"
 
 struct Aresta {
-  void *info;
+  InfoG info;
 
   char *label_partida;
   char *label_destino;
 };
 
-static struct Aresta *create_aresta(void *info, char *label_partida, char *label_destino) {
+static struct Aresta *create_aresta(InfoG info, char *label_partida, char *label_destino) {
   struct Aresta *this = calloc(1, sizeof(*this));
 
   this->info = info;
@@ -28,20 +28,21 @@ static struct Aresta *create_aresta(void *info, char *label_partida, char *label
   return this;
 }
 
-static void destroy_aresta(struct Aresta *this) {
+static void destroy_aresta(void *_this) {
+  struct Aresta *this = _this;
   free(this->label_partida);
   free(this->label_destino);
   free(this);
 }
 
 struct Vertice {
-  void *info;
+  InfoG info;
 
   char *label;
   HashTable arestas;
 };
 
-static struct Vertice *create_vertice(void *info, char *label) {
+static struct Vertice *create_vertice(InfoG info, char *label) {
   struct Vertice *this = calloc(1, sizeof(*this));
 
   this->info = info;
@@ -54,7 +55,8 @@ static struct Vertice *create_vertice(void *info, char *label) {
   return this;
 }
 
-static void destroy_vertice(struct Vertice *this) {
+static void destroy_vertice(void *_this) {
+  struct Vertice *this = _this;
   HashTable_t.destroy(this->arestas, destroy_aresta, 0);
 
   free(this->label);
@@ -64,6 +66,7 @@ static void destroy_vertice(struct Vertice *this) {
 
 struct GrafoD {
   int num_vertices;
+  int max_vertices;
 
   KDTree vertices;
   HashTable label_x_vertice;
@@ -85,7 +88,8 @@ static bool __validar_grafo(struct GrafoD *this) {
 static GrafoD __create_grafod(int numero_vertices) {
   struct GrafoD *this = calloc(1, sizeof(*this));
 
-  this->num_vertices = numero_vertices;
+  this->num_vertices = 0;
+  this->max_vertices = numero_vertices;
   this->vertices     = NULL;
 
   int total_hashtable = (int) (numero_vertices / 0.7) + 1;
@@ -113,21 +117,24 @@ static void __definir_funcoes_grafod(GrafoD _this,
   ) {
   struct GrafoD *this = (struct GrafoD *) _this;
 
+  if (this->vertices != NULL)
+    return;
+
   this->vertices = KDTree_t.create(2, check_equal, compare_x, comapre_y);
   
 }
 
 /* ===== FUNCOES ARESTA ===== */
 
-static void __insert_aresta_grafod(GrafoD this, char *node1, char *node2) {}
+static void __insert_aresta_grafod(GrafoD _this, char *node1, char *node2) {}
 
-static void __define_info_aresta_grafod(GrafoD this, char *node1, char *node2, InfoG info) {}
+static void __define_info_aresta_grafod(GrafoD _this, char *node1, char *node2, InfoG info) {}
 
-static InfoG __get_info_aresta_grafod(GrafoD this, char *node1, char *node2) {}
+static InfoG __get_info_aresta_grafod(GrafoD _this, char *node1, char *node2) {}
 
-static void __remove_aresta_grafod(GrafoD this, char *node1, char *node2) {}
+static void __remove_aresta_grafod(GrafoD _this, char *node1, char *node2) {}
 
-static bool __adjacente_grafod(GrafoD this, char *node1, char *node2) {}
+static bool __adjacente_grafod(GrafoD _this, char *node1, char *node2) {}
 
 /* ===== FUNCOES VERTICE ===== */
 
@@ -135,11 +142,11 @@ static bool __adjacente_grafod(GrafoD this, char *node1, char *node2) {}
 
 static void __define_info_vertice_grafod(GrafoD this, char *node, InfoG info) {}
 
-static InfoG __get_info_vertice_grafod(GrafoD this, char *node) {}
+static InfoG __get_info_vertice_grafod(GrafoD _this, char *node) {}
 
-static void __remove_vertice_grafod(GrafoD this, char *node) {}
+static void __remove_vertice_grafod(GrafoD _this, char *node) {}
 
-static Lista __adjacentes_grafod(GrafoD this, char *node) {}
+static Lista __adjacentes_grafod(GrafoD _this, char *node) {}
 
 const struct GrafoD_t GrafoD_t = { //
   .create              = &__create_grafod,
