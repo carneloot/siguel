6a463380d3d1a44720fdfbd13edb6189a79d1ce5
Ragnaranger <danbranlent@yahoo.com.br>
2018-11-16 12:15:22 -0200

    Preparando comando detectar carro

---
+++ src/control/comandos/qry/comando_qry_dc.c
@@ -0,0 +1,25 @@
+// Entrada:   sufixo
+/* Descrição: Detecta colisão. Trecho da rua onde ocorreu colisão fica interditado.
+              Saída .txt: placa dos carros que colidiram 
+              Nome-base-sufixo.svg */
+
+
+int comando_qry_dc( void* _this, void* _controlador ){
+  struct Comando *this = _this;
+  struct Controlador *controlador = _controlador;
+
+  /* Ordenar a lista de carros
+      -Método a definir
+
+    detectar colisões de forma eficiente
+    Guardar colisões em uma lista
+    Adicionar colisões em drawables
+      -Retângulos transparentes com borda pontilhada
+
+    criar um svg com tudo
+
+  */
+  
+
+
+}
--- src/control/comandos/qry/danielQry_dc.c
+++
@@ -1,6 +0,0 @@
-// Entrada:   sufixo
-/* Descrição: Detecta colisão. Trecho da rua onde ocorreu colisão fica interditado.
-              Saída .txt: placa dos carros que colidiram 
-              Nome-base-sufixo.svg */
-
-
