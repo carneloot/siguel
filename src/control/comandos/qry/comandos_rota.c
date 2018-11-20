edf430aabed6c81c8631a869cd16334fb75600ca
Ragnaranger <danbranlent@yahoo.com.br>
2018-11-20 18:23:47 -0200

    Transferindo comando p para arquivo comandos_rota.c

--- src/control/comandos/qry/comandos_rota.c
+++ src/control/comandos/qry/comandos_rota.c
@@ -169,9 +169,44 @@ static void escrever_caminho_txt(Lista saida, Lista caminho, GrafoD mapa) {
  */
 
 int comando_qry_p(void *_this, void *_controlador) {
-  // struct Comando *this = _this;
-  // struct Controlador *controlador = _controlador;
+  struct Comando *this            = (struct Comando *) _this;
+  struct Controlador *controlador = (struct Controlador *) _controlador;
 
+  char** params = this->params;
+  
+  // parâmetro que diz se quer representação pictória ou textual
+  char* eh_pictorico = params[0];
+
+  if( eh_pictorico[0] == 'p' ){
+    // ================================= É pictórico =================================
+    char* sufixo = params[1];
+
+    char* menor_tempo = params[2];
+    if( menor_tempo[0] == 'T'){
+      // TODO ------- Encontrar o caminho de menor tempo ---------------------------
+      
+
+      return 1;
+    }
+    // TODO -------- Encontrar o caminho de menor distância ----------------------------
+
+
+    return 1;
+  }
+
+  // ================================ Não é pictórico, é textual ================================
+
+  // Significa que o próximo parâmetro é o que define se
+  //  é para pegar o caminho mais curto ou mais rápido
+  char* menor_tempo = params[1];
+
+  if( menor_tempo[0] == 'T' ){
+    // TODO ---- Procurar o caminho com o menor tempo----------------------------------
+    return 1;
+  }
+
+  
+  // TODO-------Procurar caminho mais curto (menor distância)---------------------------------------
   return 1;
 }
 
